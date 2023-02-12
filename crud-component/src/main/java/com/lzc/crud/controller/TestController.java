36f9a470c098d5371efc86442d3d1cafef3eb161
芦泽春 <2653187194@qq.com>
2023-02-12 17:13:52 +0800

    second Version

--- crud-component/src/main/java/com/lzc/crud/controller/TestController.java
+++ crud-component/src/main/java/com/lzc/crud/controller/TestController.java
@@ -3,5 +3,6 @@ package com.lzc.crud.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("first version");
+        System.out.println("Second version");
     }
 }
